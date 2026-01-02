d496c8d32770447abc68e55b5a1e711bcb4f8461
Viraj Sule <vsule@Virajs-MacBook-Air.local>
2026-01-01 22:05:55 -0600

    update config

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
+import pathlib
 
-load_dotenv()  # Reads .env file
+dotenv_path = pathlib.Path(__file__).parent / ".env"
+load_dotenv(dotenv_path=dotenv_path)  # Reads .env file
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
